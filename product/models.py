98997f6358c42e09ae79c41499c1d840de0393b6
KIM HAJIN <rlagkwls629@gmail.com>
2022-06-03 18:11:20 +0900

    second

--- product/models.py
+++ product/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
 # Create your models here.
-
 class Drink(models.Model):
     name = models.CharField(max_length=15, null=False)
-    drink_category = models.CharField(max_length=15, null=False)
+    category = models.ForeignKey('category', on_delete=models.CASCADE)
 
 
 class Category(models.Model):
-    category = models.ForeignKey(Drink, on_delete=models.CASCADE)
+    name = models.CharField(max_length=20)
 
--- product/views.py
+++ product/views.py
@@ -1,15 +1,15 @@
-from django.shortcuts import render
-from .models import Drink, Category
-from time_att.product.models import Drink
 
+from django.shortcuts import render
+from .models import Category, Drink
 # Create your views here.
 def home_view(requset):
     if requset.method == 'POST':
-        col = requset.POST.get('col', None)
-        ese = requset.POST.get('ese', None)
-        bld = requset.POST.get('bld', None)
 
-        me = Drink.objects.all()
-        if col:
-            col_me = me.objects.get(category)
+        category = requset.POST.get('category', None)
+        all = Category.objects.get(id=category)
+        all_drink = Drink.objects.filter(category=all).values()
+        
+        return render(requset,'index.html',{'all_drink':all_drink})
+
+    elif requset.method == 'GET':
         return render(requset,'index.html')
